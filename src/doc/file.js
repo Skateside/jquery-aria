5291ae1fd083ff674e83fef7957ff853c341da26
Skateside <sk85ide@hotmail.com>
2017-02-27 22:43:28 +0000

    Updating jQuery#identify() to match #3

--- src/doc/file.js
+++ src/doc/file.js
@@ -4,27 +4,36 @@
  * attributes. Unlike other plugins that do similar things, this plugin has been
  * designed to match jQuery's style making it much easier to pick up. The plugin
  * includes:
+ * <br><br>
+ * <strong>Getting and Setting WAI-ARIA Attributes</strong>
  * <br>[jQuery#aria]{@link external:jQuery#aria} for getting and setting
  * WAI-ARIA attributes.
  * <br>[jQuery#ariaRef]{@link external:jQuery#ariaRef} for getting and setting
  * references to other elements.
  * <br>[jQuery#ariaState]{@link external:jQuery#ariaState} for getting and
  * setting states.
- * <br>[jQuery#removeAria]{@link external:jQuery#removeAria},
+ * <br><br>
+ * <strong>Removing WAI-ARIA Attributes</strong>
+ * <br>[jQuery#removeAria]{@link external:jQuery#removeAria} for removing
+ * WAI-ARIA attributes (aliased as
  * [jQuery#removeAriaRef]{@link external:jQuery#removeAriaRef} and
- * [jQuery#removeAriaState]{@link external:jQuery#removeAriaState} for removing
- * WAI-ARIA attributes.
- * <br>[jQuery#ariaVisible]{@link external:jQuery#ariaVisible} and
- * [jQuery#ariaFocusable]{@link external:jQuery#ariaFocusable} for toggling
- * common features.
- * <br>[jQuery#identify]{@link external:jQuery#identify} for generating element
- * IDs as necessary.
+ * [jQuery#removeAriaState]{@link external:jQuery#removeAriaState}).
+ * <br><br>
+ * <strong>Manipulating Landmarks</strong>
  * <br>[jQuery#role]{@link external:jQuery#role},
  * [jQuery#addRole]{@link external:jQuery#addRole} and
  * [jQuery#removeRole]{@link external:jQuery#removeRole} handling WAI-ARIA
  * landmarks.
+ * <br><br>
+ * <strong>Helper Functions for Common Functionality</strong>
+ * <br>[jQuery#identify]{@link external:jQuery#identify} for generating element
+ * IDs as necessary.
+ * <br>[jQuery#ariaVisible]{@link external:jQuery#ariaVisible} and
+ * [jQuery#ariaFocusable]{@link external:jQuery#ariaFocusable} for toggling
+ * common features.
  * <br>[jQuery.normaliseAria]{@link external:jQuery.normaliseAria} for
- * simplifying the WAI-ARIA attributes
+ * simplifying the WAI-ARIA attributes (aliased as
+ * [jQuery.normalizeAria]{@link external:jQuery.normalizeAria}).
  * <br><br>
  * The files can be downloaded on
  * [GitHub]{@link https://github.com/Skateside/jquery-aria}.
